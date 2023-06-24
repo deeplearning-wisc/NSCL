8a7293b5d6aa11df4fc757c41867f58574162245
Yiyou Sun <sunyiyou@cs.wisc.edu>
2023-06-24 09:34:48 -0500

    update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
+python run_ncd.py --config-file configs/supspectral_resnet_mlp1000_norelu_cifar100.yaml --labeled-num 80 --deep_eval_freq 20
+python run_ncd.py --config-file configs/supspectral_resnet_mlp1000_norelu_cifar100.yaml --labeled-num 50 --deep_eval_freq 20
+python run_ncd.py --config-file configs/supspectral_resnet_mlp1000_norelu_cifar10.yaml --labeled-num 5 --deep_eval_freq 20
 
-python run_ncd.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 80 --deep_eval_freq 20 --layer penul
-python run_ncd.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 50 --deep_eval_freq 20 --layer penul
-python run_ncd.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar10_lr003_mu1.yaml --labeled-num 5 --deep_eval_freq 20 --layer penul
-
-python run_unsup.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 80 --deep_eval_freq 20 --layer penul
-python run_unsup.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 50 --deep_eval_freq 20 --layer penul
-python run_unsup.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar10_lr003_mu1.yaml --labeled-num 5 --deep_eval_freq 20 --layer penul
+#python run_ncd.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 80 --deep_eval_freq 20 --layer penul
+#python run_ncd.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 50 --deep_eval_freq 20 --layer penul
+#python run_ncd.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar10_lr003_mu1.yaml --labeled-num 5 --deep_eval_freq 20 --layer penul
+#
+#python run_unsup.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 80 --deep_eval_freq 20 --layer penul
+#python run_unsup.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar100_lr003_mu1.yaml --labeled-num 50 --deep_eval_freq 20 --layer penul
+#python run_unsup.py --config-file configs/spectral_resnet_mlp1000_norelu_cifar10_lr003_mu1.yaml --labeled-num 5 --deep_eval_freq 20 --layer penul
